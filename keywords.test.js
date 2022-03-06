f406c4f3bbf1826e18c4c317ad5f959feb8f90f5
Thamir Qadah <qadah.thamir@gmail.com>
2022-03-06 14:42:06 +0300

    Fixed local testing on Windows

--- keywords.test.js
+++ keywords.test.js
@@ -7,6 +7,6 @@ test('Can scan and generate correct tokens for keywords', function() {
         {encoding:'utf8', flag:'r'});
     var output = parser.parse(input);
     var outputString = JSON.stringify(output, null, 2)
-    var outputJson = fs.readFileSync(outputJsonFile, {encoding: 'utf-8', flag:'r'});    
+    var outputJson = fs.readFileSync(outputJsonFile, {encoding: 'utf-8', flag:'r'}).replace(/\r\n|\r/g, '\n');    
     expect(outputString).toBe(outputJson);
   })
--- numbers.test.js
+++ numbers.test.js
@@ -7,6 +7,6 @@ test('can scan and generate correct tokens for numbers', function() {
         {encoding:'utf8', flag:'r'});
     var output = parser.parse(input);
     var outputString = JSON.stringify(output, null, 2)
-    var outputJson = fs.readFileSync(outputJsonFile, {encoding: 'utf-8', flag:'r'});    
+    var outputJson = fs.readFileSync(outputJsonFile, {encoding: 'utf-8', flag:'r'}).replace(/\r\n|\r/g, '\n');    
     expect(outputString).toBe(outputJson);
   })
